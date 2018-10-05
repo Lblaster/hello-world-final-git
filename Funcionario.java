97ddc5cd8605ca49c286f295c1b60f0685a5181b
Lucas de Avila Mendes <201819060616@lab03-l35.dominio.facom.ufms.br>
2018-10-05 10:23:15 -0400

    novo arquivo

---
+++ Funcionario.java
@@ -0,0 +1,35 @@
+package javaapplication1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 201819060616
+ */
+public class Funcionario {
+    String nome;
+    String departamento;
+    double salario;
+    String dataEntrada;
+    String rg;
+    
+    public Funcionario(){
+        Data dataEntrada = new Data();
+    }
+    
+    
+    public void recebeAumento(double aumento){
+        
+    }
+
+    public void calculaGanhoAnual(){
+        
+    }
+    
+    
+    
+}
